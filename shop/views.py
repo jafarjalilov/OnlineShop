7c82fb90ef686bb24106d0f027274a73fc085e3e
Jalilov Jafar <65230803+jafarjalilov@users.noreply.github.com>
2021-11-01 19:28:32 +0500

    Product list & detail pages

---
+++ shop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+
+    template = 'shop/product/list.html'
+    context = {'category': category, 'categories':categories, 'products':products}
+    return render(request, template, context)
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+
+    template = 'shop/product/detail.html'
+    context = {'product':product}
+    return render(request, template, context)
